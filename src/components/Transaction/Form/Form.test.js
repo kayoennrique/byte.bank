86895a70ddf39fa36a14483967719c46f3f32fb8
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-01 14:24:11 -0300

    Simulating events

--- src/components/Transaction/Form/Form.test.js
+++ src/components/Transaction/Form/Form.test.js
@@ -22,3 +22,27 @@ describe('Must render an input field', () => {
         expect(textField).toHaveValue(50);
     });
 });
+
+test('You must call an onSubmit event when you click on perform transaction', () => {
+    const carryOutTransaction = jest.fn();
+
+    render(<Form carryOutTransaction={carryOutTransaction}/>);
+    const button = screen.getByRole('button');
+
+    userEvent.click(button);
+    expect(carryOutTransaction).toHaveBeenCalledTimes(1);
+  });
+
+  test('It must be possible to select an element option <select/>', () => {
+    render(<Form />);
+    const select = screen.getByRole('combobox'); 
+    userEvent.selectOptions(select, ['Depósito']); 
+
+    expect(
+      screen.getByRole('option', { name: 'Selecione um tipo de transação' })
+        .selected
+    ).toBe(false); 
+    expect(screen.getByRole('option', { name: 'Depósito' }).selected).toBe(
+      true
+    ); 
+  });  

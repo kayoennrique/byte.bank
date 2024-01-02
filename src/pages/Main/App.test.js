133308b53ec51f2e7102c0c95731437bb0f8037a
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-02 13:47:00 -0300

    Integration between pages

--- src/pages/Main/App.test.js
+++ src/pages/Main/App.test.js
@@ -2,23 +2,35 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import AppRoutes from '../../routes';
 
 describe('Component <App/>', () => {
-    it('It should allow adding a transaction to Statement', () => {
-      render(<App />, { wrapper: BrowserRouter });
+  test('It should allow adding a transaction to Statement', () => {
+    render(<App />, { wrapper: BrowserRouter });
 
-      const select = screen.getByRole('combobox');  
-      const fieldValue = screen.getAllByPlaceholderText('Digite um valor');
-      const button = screen.getByRole('button');
+    const select = screen.getByRole('combobox');
+    const fieldValue = screen.getByPlaceholderText('Digite um valor');
+    const button = screen.getByRole('button');
 
-      userEvent.selectOptions(select, ['Depósito']);
-      userEvent.type(fieldValue, '100');
-      userEvent.click(button);
+    userEvent.selectOptions(select, ['Depósito']);
+    userEvent.type(fieldValue, '100');
+    userEvent.click(button);
 
-      const newTransaction = screen.getByTestId('transaction-list');
-      const itemExtract = screen.getByRole('listitem');
+    const newTransaction = screen.getByTestId('transaction-list');
+    const itemExtract = screen.getByRole('listitem');
 
-      expect(newTransaction).toContainElement(itemExtract);
-       
+    expect(newTransaction).toContainElement(itemExtract);
+  });
+
+  test('You must navigate to the page corresponding to the clicked link', async () => {
+    render(<AppRoutes />, { wrapper: BrowserRouter });
+
+    const linkPageCards = screen.getByText('Cartões');
+    expect(linkPageCards).toBeInTheDocument();
+
+    userEvent.click(linkPageCards);
+
+    const titlePageCards = await screen.findByText('Meus cartões');
+    expect(titlePageCards).toBeInTheDocument();
   });
 });

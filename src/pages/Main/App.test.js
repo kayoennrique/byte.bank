b1c018dcdf6f6625350ca1917f8169b7fc31a76f
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-02 13:52:01 -0300

    Integrating user action by clicking the link

--- src/pages/Main/App.test.js
+++ src/pages/Main/App.test.js
@@ -5,7 +5,7 @@ import App from './App';
 import AppRoutes from '../../routes';
 
 describe('Component <App/>', () => {
-  test('It should allow adding a transaction to Statement', () => {
+  it('It should allow adding a transaction to Statement', () => {
     render(<App />, { wrapper: BrowserRouter });
 
     const select = screen.getByRole('combobox');
@@ -22,7 +22,7 @@ describe('Component <App/>', () => {
     expect(newTransaction).toContainElement(itemExtract);
   });
 
-  test('You must navigate to the page corresponding to the clicked link', async () => {
+  it('You must navigate to the page corresponding to the clicked link', async () => {
     render(<AppRoutes />, { wrapper: BrowserRouter });
 
     const linkPageCards = screen.getByText('Cartões');
@@ -33,4 +33,17 @@ describe('Component <App/>', () => {
     const titlePageCards = await screen.findByText('Meus cartões');
     expect(titlePageCards).toBeInTheDocument();
   });
+
+  it('You must navigate to the page corresponding to the clicked link', async () => {
+    render(<AppRoutes />, { wrapper: BrowserRouter });
+
+    const linkPageInvestments = screen.getByText('Investimentos');
+    expect(linkPageInvestments).toBeInTheDocument();
+
+    userEvent.click(linkPageInvestments);
+
+    const titlePageInvestments = await screen.findByText('Renda Fixa');
+    expect(titlePageInvestments).toBeInTheDocument();
+  });
+
 });

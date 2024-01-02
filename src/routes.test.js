8d952571a6c10c2a8747977681f8ec581e7322f8
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-02 03:03:54 -0300

    Controlling history

--- src/routes.test.js
+++ src/routes.test.js
@@ -1,11 +1,28 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter, MemoryRouter, Routes, Route } from 'react-router-dom';
 import App from './pages/Main/App';
-import { BrowserRouter } from 'react-router-dom';
+import Cards from './components/Cards';
 
 describe('Routes', () => {
     it('Should render the main route', () => {
-        render(<App />, {wrapper: BrowserRouter});
-        const user = screen.getByText('Olá, Kayo :)!');
-        expect(user).toBeInTheDocument();
+      render(<App />, { wrapper: BrowserRouter });
+      const user = screen.getByText('Olá, Kayo :)!');
+      expect(user).toBeInTheDocument();
     });
-  });
+  
+    it('Should render the Cards route', () => {
+      const route = '/cards';
+      render(
+        <MemoryRouter initialEntries={[route]}>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route path="cards" element={<Cards />} />
+            </Route>
+          </Routes>
+        </MemoryRouter>
+      );
+  
+      const meusCartoes = screen.getByText('Meus cartões');
+      expect(meusCartoes).toHaveTextContent('Meus cartões');
+    });
+});

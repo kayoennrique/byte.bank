c112ac24f54a15b67e6af6d7a335810c60cf2fe6
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-02 14:35:47 -0300

    Integrating tests for POST requests

--- src/services/integrations.test.js
+++ src/services/integrations.test.js
@@ -1,5 +1,5 @@
 import api from './api';
-import { searchTransactions } from './transactions';
+import { searchTransactions, saveTransaction } from './transactions';
 
 jest.mock('./api');
 
@@ -23,7 +23,7 @@ const mockTransaction = [
       });
     };
 
-  const mockRequisitionErro = (recurrence) => {
+  const mockRequisitionErro = () => {
     return new Promise((_, reject) => {
         setTimeout(() => {
           reject();
@@ -31,21 +31,35 @@ const mockTransaction = [
       });
     };
 
-describe('Requisições para API', () => {
+  const mockRequisitionPost = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          status: 201,
+        });
+      }, 200);
+    });
+  };    
+
+describe('API requests', () => {
     it('Must return a list of transactions', async () => {
         api.get.mockImplementation(() => mockRequisition(mockTransaction));
-
         const transactions = await searchTransactions();
         expect(transactions).toEqual(mockTransaction);
         expect(api.get).toHaveBeenCalledWith('/transactions')
     });
 
     it('Must return an empty list when the request fails', async () => {
-        api.get.mockImplementation(() => mockRequisitionErro());
-
-        const transactions = await searchTransactions();
-
-        expect(transactions).toEqual([]);
-        expect(api.get).toHaveBeenCalledWith('/transactions')
+      api.post.mockImplementation(() => mockRequisitionErro());
+      const status = await saveTransaction(mockTransaction[0]);
+      expect(status).toBe('Erro na requisição');
+      expect(api.post).toHaveBeenCalledWith('/transactions', mockTransaction[0]);
     });
+
+    it('Must return a status of 201 - (Created) after a POST request', async () => {
+      api.post.mockImplementation(() => mockRequisitionPost());
+      const status = await saveTransaction(mockTransaction[0]);
+      expect(status).toBe(201);
+      expect(api.post).toHaveBeenCalledWith('/transactions', mockTransaction[0]);
+    });    
 });

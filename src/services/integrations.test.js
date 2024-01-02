0d4ef448eb36ce636f387698ed25c7155fa8c233
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-02 14:16:14 -0300

    Testing error requests

--- src/services/integrations.test.js
+++ src/services/integrations.test.js
@@ -6,10 +6,10 @@ jest.mock('./api');
 const mockTransaction = [
     {
       id: 1,
-      transacao: 'Depósito',
-      valor: '100',
+      transaction: 'Depósito',
+      amount: '100',
       data: '22/11/2022',
-      mes: 'Novembro',
+      month: 'Novembro',
     },
   ];
 
@@ -23,6 +23,14 @@ const mockTransaction = [
       });
     };
 
+  const mockRequisitionErro = (recurrence) => {
+    return new Promise((_, reject) => {
+        setTimeout(() => {
+          reject();
+        }, 200);
+      });
+    };
+
 describe('Requisições para API', () => {
     it('Must return a list of transactions', async () => {
         api.get.mockImplementation(() => mockRequisition(mockTransaction));
@@ -30,5 +38,14 @@ describe('Requisições para API', () => {
         const transactions = await searchTransactions();
         expect(transactions).toEqual(mockTransaction);
         expect(api.get).toHaveBeenCalledWith('/transactions')
-    })
+    });
+
+    it('Must return an empty list when the request fails', async () => {
+        api.get.mockImplementation(() => mockRequisitionErro());
+
+        const transactions = await searchTransactions();
+
+        expect(transactions).toEqual([]);
+        expect(api.get).toHaveBeenCalledWith('/transactions')
+    });
 });

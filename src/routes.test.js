9ab9aea14be808c57e9520e601744c84e7ae348d
Kayo Ennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-02 15:40:29 -0300

    Update routes.test.js

--- src/routes.test.js
+++ src/routes.test.js
@@ -1,17 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter, MemoryRouter, Routes, Route } from 'react-router-dom';
-import App from './pages/main/App';
+import App from './pages/Main/App';
 import AppRoutes from './routes';
 import Cards from './components/Cards';
 
 describe('Routes', () => {
-  it('Should render the main route', () => {
+  it('Should render the main route () => {
     render(<App />, { wrapper: BrowserRouter });
     const user = screen.getByText('Olá, Kayo :)!');
     expect(user).toBeInTheDocument();
   });
 
-  it('Deve renderizar a rota Cartões', () => {
+  it('Should render the Cards route', () => {
     const route = '/cards';
     render(
       <MemoryRouter initialEntries={[route]}>
@@ -25,6 +25,18 @@ describe('Routes', () => {
 
     const myCards = screen.getByText('Meus cartões');
     expect(myCards).toHaveTextContent('Meus cartões');
+  });
+
+  it('Should render current route location', () => {
+    const route = '/cards';
+    render(
+      <MemoryRouter initialEntries={[route]}>
+        <App />
+      </MemoryRouter>
+    );
+
+    const currentLocation = screen.getByTestId('local');
+    expect(currentLocation).toHaveTextContent(route);
   });
 
   it('Should render 404 page', () => {

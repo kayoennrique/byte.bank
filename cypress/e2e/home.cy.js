f40bccd04f38b8419da0b7ea3e99746c67f1d86f
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-03 01:44:16 -0300

    Date attributes

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,4 +1,8 @@
-it('Should correctly render perks section text ', () => {
-  cy.visit('http://localhost:3000/');
-  cy.get('h1').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+describe('Start', () => {
+  it('Should render the h1 with the correct text', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('[data-test="main-title"]').contains(
+      'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
+    );
+  });
 });

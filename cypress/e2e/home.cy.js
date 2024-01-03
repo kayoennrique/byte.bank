67a2fcc7c724cf736ce83b773dc321089c4af997
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-03 01:36:39 -0300

    Debugging error

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,4 +1,4 @@
 it('Should correctly render perks section text ', () => {
   cy.visit('http://localhost:3000/');
-  cy.get('h2').contains('Advantages of our bank:');
+  cy.get('h1').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
 });

45c2ffe7b92dcbe6a4a2932d17b46e21a365e7d4
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-03 14:04:47 -0300

    Accessing other pages

--- cypress/e2e/pages.cy.js
+++ cypress/e2e/pages.cy.js
@@ -1,5 +1,12 @@
-describe('Testing multiple pages', () => {
-    it('You should be able to access the cards page', ()=>{
-      cy.visit('/')
-    });
+describe('Testing multiple pages', () => { 
+  it('You should be able to access the cards page', ()=>{
+    cy.visit('/');
+    cy.getByData('button-login').click();
+    cy.getByData('email-input').type('kayo.ennrique@hotmail.com.br');
+    cy.getByData('password-input').type('123456');
+    cy.getByData('button-toSend').click();
+
+    cy.getByData('app-home').find('a').eq(1).click();
+    cy.getByData('title-cards').should('exist').and('have.text', 'Meus cartões');
   });
+ });

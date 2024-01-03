7cb1496b382e7ff843f602f55b5d71b3fbba59b0
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-03 14:09:49 -0300

    Testing url and path

--- cypress/e2e/pages.cy.js
+++ cypress/e2e/pages.cy.js
@@ -6,7 +6,11 @@ describe('Testing multiple pages', () => {
     cy.getByData('password-input').type('123456');
     cy.getByData('button-toSend').click();
 
+    cy.location('pathname').should('eq','/home');
+
     cy.getByData('app-home').find('a').eq(1).click();
     cy.getByData('title-cards').should('exist').and('have.text', 'Meus cartões');
+
+    cy.location('pathname').should('eq', '/home/cards');
   });
- });
+});

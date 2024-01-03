b1ddfa0ed1655672a05b7d4059f0ef9620a5555d
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-03 02:43:45 -0300

    Submitting empty form

--- cypress/e2e/loginForm.cy.js
+++ cypress/e2e/loginForm.cy.js
@@ -1,13 +1,20 @@
 describe('Login Form', ()=>{
     beforeEach(()=>{
       cy.visit('http://localhost:3000')
-    })
+    });
   
-    it('Não deve permitir um email inválido', ()=>{
-      cy.getByData('button-login').click()
-      cy.getByData('email-input').type('neilton@alura')
-      cy.getByData('password-input').type('123456')
+    it('Should not allow an invalid email', ()=>{
+      cy.getByData('button-login').click();
+      cy.getByData('email-input').type('kayo@hotmail');
+      cy.getByData('password-input').type('123456');
       cy.getByData('button-toSend').click()
-      cy.getByData('message-erro').should('exist').and('have.text', 'O email digitado é inválido')
-    })
-  })
+      cy.getByData('message-erro').should('exist').and('have.text', 'O email digitado é inválido');
+    });
+
+    it.only('Should not allow a blank field', ()=>{
+        cy.getByData('button-login').click();
+        cy.getByData('password-input').type('123456');
+        cy.getByData('button-toSend').click()
+        cy.getByData('message-erro').should('exist').and('have.text', 'O campo email é obrigatório');
+      });
+    });

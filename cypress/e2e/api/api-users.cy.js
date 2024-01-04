e240631cded85a7d6cbc3f08e275ae1ebc5f9eeb
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-04 00:54:22 -0300

    Testing API flaws

--- cypress/e2e/api/api-users.cy.js
+++ cypress/e2e/api/api-users.cy.js
@@ -1,11 +1,33 @@
-describe('Making requests to the API',()=>{
-    context('GET /users',()=>{
-        it('Should return a list of users', ()=>{
-            cy.request('GET', 'http://localhost:8000/users').then((response)=>{
-                expect(response.status).to.eq(200);
-                  expect(response.body).length.to.be.greaterThan(1);  
-            });
-          });
+describe('Making requests to the API', () => {
+    context('GET /users', () => {
+      it('Should return a list of users', () => {
+        cy.request('GET', 'http://localhost:8000/users').then((response) => {
+          expect(response.status).to.eq(200);
+          expect(response.body).length.to.be.greaterThan(1);
+        });
       });
-});
+    });
   
+    context('GET /users/:userId', () => {
+      it('Must return a single user', () => {
+        cy.request({
+          method: 'GET',
+          url: 'http://localhost:8000/users/40a41438-84a6-4b4d-ae1d-7f1713d0a9fe',
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+          expect(response.body).to.have.property('name');
+        });
+      });
+  
+      it('Should return an error when the user is invalid', () => {
+        cy.request({
+          method: 'GET',
+          url: 'http://localhost:8000/users/40a41438-84a6-4b4d',
+          failOnStatusCode: false,
+        }).then((response) => {
+          expect(response.status).to.eq(404);
+          expect(response.body).to.eq('Not Found');
+        });
+      });
+    });
+});

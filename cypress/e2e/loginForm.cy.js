7292fb9007fc80333f427137d5b54d4a0b5ba271
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-04 00:23:53 -0300

    Creating the login command

--- cypress/e2e/loginForm.cy.js
+++ cypress/e2e/loginForm.cy.js
@@ -1,20 +1,24 @@
 describe('Login Form', ()=>{
-    beforeEach(()=>{
-      cy.visit('http://localhost:3000')
-    });
+  beforeEach(()=>{
+      cy.login('kayo.ennrique@hotmail.com.br','123456')
+  })
+
+  it.only('You must access the home page', ()=>{
+      cy.getByData('title-welcome').should('contain', 'Bem vindo de volta!');
+  })
   
-    it('Should not allow an invalid email', ()=>{
+  it('Should not allow an invalid email', ()=>{
       cy.getByData('button-login').click();
-      cy.getByData('email-input').type('kayo@hotmail');
+      cy.getByData('email-input').type('kayo.ennrique@hotmail.com.br');
       cy.getByData('password-input').type('123456');
       cy.getByData('button-toSend').click()
       cy.getByData('message-erro').should('exist').and('have.text', 'O email digitado é inválido');
-    });
+  });
 
-    it.only('Should not allow a blank field', ()=>{
+  it('Should not allow a blank field', ()=>{
         cy.getByData('button-login').click();
         cy.getByData('password-input').type('123456');
         cy.getByData('button-toSend').click()
         cy.getByData('message-erro').should('exist').and('have.text', 'O campo email é obrigatório');
       });
-    });
+  });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,16 @@
- Cypress.Commands.add('getByData', (selector) => {
+Cypress.Commands.add('getByData', (selector) => {
     return cy.get(`[data-test=${selector}]`);
- });
+});
 
- Cypress.Commands.add('checkText', (selector, text) => {
+Cypress.Commands.add('login', (email, password) => {
+  cy.visit('/')
+  cy.getByData('button-login').click();
+  cy.getByData('email-input').type(email);
+  cy.getByData('password-input').type(password);
+  cy.getByData('button-toSend').click()
+  cy.url().should('contain', '/home')
+});
+
+Cypress.Commands.add('checkText', (selector, text) => {
    cy.get(`${selector}`).contains(`${text}`)
- });
+});

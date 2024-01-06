7050c68d3f885e1158521697d8434d88069d7978
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-06 01:10:32 -0300

    Using dynamic data

--- cypress/e2e/formLogin.cy.js
+++ cypress/e2e/formLogin.cy.js
@@ -1,23 +1,39 @@
 describe('Login Form', () => {
-
   it.only('You must access the home page', () => {
+    cy.fixture('users').then((user) => {
+    cy.login(user[0].email, user[0].senha);
+    cy.visit('/home');
+    cy.url().should('include', '/home');
+    cy.getByData('titulo-boas-vindas').should(
+        'contain',
+        'Bem vindo de volta!'
+      );
+    cy.contains(user[0].nome).should('be.visible');
+    });
+  });
+
+  it('You must access the home page', () => {
     cy.login('kayo.ennrique@hotmail.com.br', '123456');
-    cy.visit('/home')
+    cy.visit('/home');
     cy.getByData('titulo-boas-vindas').should('contain', 'Bem vindo de volta!');
   });
-  
+
   it('Should not allow an invalid email', () => {
     cy.getByData('botao-login').click();
     cy.getByData('email-input').type('kayo.ennrique@hotmail.com.br');
     cy.getByData('senha-input').type('123456');
-    cy.getByData('botao-enviar').click()
-    cy.getByData('message-erro').should('exist').and('have.text', 'O email digitado é inválido');
+    cy.getByData('botao-enviar').click();
+    cy.getByData('mensagem-erro')
+      .should('exist')
+      .and('have.text', 'O email digitado é inválido');
   });
 
   it('Should not allow a blank field', () => {
     cy.getByData('botao-login').click();
     cy.getByData('senha-input').type('123456');
-    cy.getByData('botao-enviar').click()
-    cy.getByData('message-erro').should('exist').and('have.text', 'O campo email é obrigatório');
+    cy.getByData('botao-enviar').click();
+    cy.getByData('mensagem-erro')
+      .should('exist')
+      .and('have.text', 'O campo email é obrigatório');
   });
 });
